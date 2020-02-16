b7851f350cc30f2fa7131b8cf1f28b272c44f1ef
Levi Zitting <me@levizitting.com>
2020-02-16 11:43:06 -0600

    Add firebase and update integrate with ios app

--- lib/widgets/app_navigator.dart
+++ lib/widgets/app_navigator.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
 
 class AppNavigator extends StatelessWidget {
+  static var analytics = FirebaseAnalytics();
+  static var analyticsObserver = FirebaseAnalyticsObserver(
+    analytics: analytics,
+  );
   static final _navigatorKey = GlobalKey<NavigatorState>();
+
   final _RouteChangeCallBack onRouteChange;
   final Map<String, WidgetBuilder> routes;
   final String initialRoute;
@@ -31,7 +38,8 @@ class AppNavigator extends StatelessWidget {
         key: _navigatorKey,
         initialRoute: initialRoute,
         observers: [
-          _AppNavigationObserver(onRouteChange: _onRouteChangeHandler)
+          _AppNavigationObserver(onRouteChange: _onRouteChangeHandler),
+          analyticsObserver,
         ],
         onUnknownRoute: (settings) {
           return MaterialPageRoute(
