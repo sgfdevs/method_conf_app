9d8b1eadb1c931f68693fca99294235032916b94
Levi Zitting <me@levizitting.com>
2020-02-16 22:23:45 -0600

    Update sentry

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,13 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_sentry/flutter_sentry.dart';
+import 'package:method_conf_app/env.dart';
 
-import 'package:method_conf_app/utils/error_tracking.dart';
 import 'package:method_conf_app/app.dart';
 
 
-void main() {
-  var errorTracker = ErrorTracker();
-
-  runZoned(
-    () => runApp(App()),
-    onError: (error, stackTrace) {
-      errorTracker.reportError(error, stackTrace);
-    },
-  );
-
-  FlutterError.onError = (FlutterErrorDetails details, {bool forceReport = false}) {
-    if (!isInDebugMode) {
-      errorTracker.reportError(details.exception, details.stack);
-    }
-
-    FlutterError.dumpErrorToConsole(details, forceReport: forceReport);
-  };
-}
+Future<void> main() => FlutterSentry.wrap(
+  () async => runApp(App()),
+  dsn: Env.sentryDsn,
+);
