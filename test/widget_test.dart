c4346c558c577d6193d52e1dddc117d6efff0576
Levi Zitting <me@levizitting.com>
2024-07-05 23:34:21 -0500

    chore: migrate to new flutter project template

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,18 +1,15 @@
 // This is a basic Flutter widget test.
 //
 // To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
+// utility in the flutter_test package. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:method_conf_app/app.dart';
 
 void main() {
-  testWidgets('App builds', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     await tester.pumpWidget(App());
   });
 }
